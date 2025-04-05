049b84de953e5c8a723317c354dfbdb144faf8aa
Anirudh Pulavarthy <123797644+anirudh-pulavarthy@users.noreply.github.com>
2025-04-05 07:26:49 -0500

    updates

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -1,25 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
-  imports: [],
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.scss'
+  styleUrls: ['./about-me.component.scss']
 })
-export class AboutMeComponent {
-
+export class AboutMeComponent implements OnInit {
   timeGreeting: string = '';
 
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
   ngOnInit(): void {
+    this.setGreeting();
+    this.observeCard();
+  }
+
+  setGreeting(): void {
     const hour = new Date().getHours();
-    if (hour >= 5 && hour < 12) {
+    if (hour < 12) {
       this.timeGreeting = 'Good morning!';
-    } else if (hour >= 12 && hour < 17) {
+    } else if (hour < 18) {
       this.timeGreeting = 'Good afternoon!';
-    } else if (hour >= 17 && hour < 21) {
-      this.timeGreeting = 'Good evening!';
     } else {
-      this.timeGreeting = 'Burning the midnight oil!';
+      this.timeGreeting = 'Good evening!';
     }
+  }
+
+  observeCard(): void {
+    const card = this.el.nativeElement.querySelector('.content-card');
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          this.renderer.addClass(card, 'animate-in');
+          observer.disconnect();
+        }
+      },
+      { threshold: 0.2 }
+    );
+    observer.observe(card);
   }
 }
